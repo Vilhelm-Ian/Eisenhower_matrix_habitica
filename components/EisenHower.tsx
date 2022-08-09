9bee6269af1e9717ec08feef1f280624cc45bb38
Jovan <mariothrowsfieball@gmail.com>
2022-08-09 11:48:35 +0200

    render tasks name in appropriate grid cell

--- components/EisenHower.tsx
+++ components/EisenHower.tsx
@@ -1,10 +1,33 @@
-export default function Login() {
+interface Props {
+	grid_data: {
+		_do: any[];
+		deligate: any[];
+		schedule: any[];
+		_delete: any[];
+	};
+}
+
+export default function EisenHower(props: Props) {
+	console.log(props.grid_data, "props");
+
+	let _do = props.grid_data._do?.map((task) => (
+		<p key={task.text}>{task.text}</p>
+	));
+	let schedule = props.grid_data.schedule?.map((task) => (
+		<p key={task.text}>{task.text}</p>
+	));
+	let deligate = props.grid_data.deligate?.map((task) => (
+		<p key={task.text}>{task.text}</p>
+	));
+	let _delete = props.grid_data._delete?.map((task) => (
+		<p key={task.text}>{task.text}</p>
+	));
 	return (
 		<div className="grid">
-			<div className="urgent_important"></div>
-			<div className="noturgent_important"></div>
-			<div className="urgent_notimportant"></div>
-			<div className="noturgent_notimportant"></div>
+			<div className="do">{_do}</div>
+			<div className="schedule">{schedule}</div>
+			<div className="deligate">{deligate}</div>
+			<div className="delete">{_delete}</div>
 		</div>
 	);
 }
